18ee513d58bbd169b15ae9f6301651a94d11070f
Fritz Symon Blancaflor <fritzsymon23@gmail.com>
2025-08-27 21:03:14 +0800

    Add files via upload

---
+++ Section 32 - Exploring Modules/language-guesser/app.js
@@ -0,0 +1,10 @@
+const rows = 5;
+let pattern = "";
+
+for (let i = 1; i <= rows; i++) {
+  for (let j = 1; j <= i; j++) {
+    pattern += "* ";
+  }
+  pattern += "\n";
+}
+console.log(pattern);
---
+++ Section 32 - Exploring Modules/language-guesser/index.js
@@ -0,0 +1,12 @@
+// import { franc } from "franc";
+// import langs from "langs";
+// import colors from "colors";
+
+// const input = process.argv[2];
+// const langCode = franc(input);
+// if (langCode === "und") {
+//   console.log(colors.red("WHAT ARE THOOOSEEE?"));
+// } else {
+//   const language = langs.where("3", langCode);
+//   console.log(colors.blue(language.name));
+// }
